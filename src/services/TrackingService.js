27e39785c43f27ad19210d199b65779c12cd04c2
Pascal Precht <pascal.precht@googlemail.com>
2014-03-10 17:16:56 +0100

    fix(trackerService): fixes broken constructor

--- src/services/TrackingService.js
+++ src/services/TrackingService.js
@@ -3,5 +3,5 @@ angular.module('sdk.services.trackingService', []);
 angular
     .module('sdk.services.trackingService')
     .factory('trackingService', ['$window', '$http', 'configService', function($window, $http, configService){
-        return new cc.TrackingService($window, $http, configService);
+        return new cc.tracking.TrackingService($window, $http, configService);
 }]);
